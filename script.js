744ab8c91e37da11ac74c6e122e128b2450dedc9
JoaoVitorLira <joao.liraves07@gmail.com>
2023-08-30 10:49:11 -0300

    javascript

--- script.js
+++ script.js
@@ -1,2 +1,100 @@
+const restartButton = document.getElementById("reiniciar");
+
+const questions = [
+    {
+        question: "Quando a Segunda Guerra Mundial chegou ao fim?",
+        options: ["1941", "1945", "1954", "1971"],
+        answer: 1,
+        image: "guerra.jpeg"
+    },
+    {
+        question: "Qual a menor partícula visível da matéria?",
+        options: ["Átomo", "Molécula", "Próton", "Elétron"],
+        answer: 0,
+        image: "atomo.jpg"
+    },
+    {
+        question: "Qual o valor de x na equação x+7 = 14",
+        options: ["3", "5", "7", "9"],
+        answer: 2,
+        image: "equacao.jpeg"
+    },
+    {
+        question: "Observando a imagem, qual o tipo de relevo presente nela?",
+        options: ["Montanha", "Depressão", "Planalto", "Vale"],
+        answer: 1,
+        image: "depressao.jpeg"
+    },
+    {
+        question: "Essa bandeira é de qual país?",
+        options: ["Eslovênia", "Croácia", "Áustria", "Eslováquia"],
+        answer: 0,
+        image: "eslovenia.jpg"
+    },
+    // Adicione mais perguntas aqui
+];
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+const questionElement = document.getElementById("question");
+const imageElement = document.getElementById("image");
+const optionElements = [
+    document.getElementById("opcao1"),
+    document.getElementById("opcao2"),
+    document.getElementById("opcao3"),
+    document.getElementById("opcao4")
+];
+const scoreElement = document.getElementById("pontuacao");
+
+function loadQuestion(index) {
+    if (index < questions.length) {
+        const currentQuestion = questions[index];
+        questionElement.textContent = currentQuestion.question;
+        imageElement.src = currentQuestion.image;
+        for (let i = 0; i < optionElements.length; i++) {
+            optionElements[i].textContent = currentQuestion.options[i];
+            optionElements[i].addEventListener("click", () => checkAnswer(i));
+        }
+    } else {
+        // Todas as perguntas foram respondidas
+        questionElement.textContent = "Parabéns! Você completou o quiz!";
+        imageElement.src = "parabens.jpeg";
+        for (const optionElement of optionElements) {
+            optionElement.style.display = "none"; // Oculta as opções
+        }
+    }
+}
+
+function checkAnswer(selectedIndex) {
+    const currentQuestion = questions[currentQuestionIndex];
+    if (selectedIndex === currentQuestion.answer) {
+        score += 10;
+        scoreElement.textContent = score;
+        currentQuestionIndex++;
+        loadQuestion(currentQuestionIndex);
+    }
+}
+
+restartButton.addEventListener("click", () => {
+    window.location.href = "index.html"; // Redireciona para a página index.html
+});
+
+loadQuestion(currentQuestionIndex);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
